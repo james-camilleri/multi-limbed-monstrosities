4390af0104661e0920db4a62628b8292f7b3e121
James Camilleri <james@james.mt>
2021-12-09 12:41:30 +0000

    Invert camera and tweak target sizes

--- unity-project/Multi Limbed Monstrosities/Assets/Scripts/BlazePose/WebCamInput.cs
+++ unity-project/Multi Limbed Monstrosities/Assets/Scripts/BlazePose/WebCamInput.cs
@@ -4,64 +4,65 @@ using UnityEngine.Events;
 
 public class WebCamInput : MonoBehaviour
 {
-    // Unused
-    string              webCamName;
-    [SerializeField] Vector2             webCamResolution = new Vector2(1920, 1080);
-    [SerializeField] Texture             staticInput;
-   
-    public           UnityEvent<Texture> OnFrameReady;
+  // Unused
+  string webCamName;
+  [SerializeField] Vector2 webCamResolution = new Vector2(1920, 1080);
+  [SerializeField] Texture staticInput;
 
-    // Provide input image Texture.
-    public Texture             inputImageTexture => staticInput != null ? staticInput : inputRT;
-   
+  public UnityEvent<Texture> OnFrameReady;
 
-    WebCamTexture webCamTexture;
-    RenderTexture inputRT;
+  // Provide input image Texture.
+  public Texture inputImageTexture => staticInput != null ? staticInput : inputRT;
 
-    void Start()
+
+  WebCamTexture webCamTexture;
+  RenderTexture inputRT;
+
+  void Start()
+  {
+    if (staticInput == null)
     {
-        if(staticInput == null)
-        {
-            if (webCamName == string.Empty)
-            {
-                webCamTexture = new WebCamTexture((int) webCamResolution.x, (int) webCamResolution.y);
-            }
-            else
-            {
-                webCamTexture = new WebCamTexture(webCamName, (int)webCamResolution.x, (int)webCamResolution.y);
-            }
-          
-            Debug.Log(webCamTexture.deviceName);
-            
-            webCamTexture.Play();
-        }
+      if (webCamName == string.Empty)
+      {
+        webCamTexture = new WebCamTexture((int)webCamResolution.x, (int)webCamResolution.y);
+      }
+      else
+      {
+        webCamTexture = new WebCamTexture(webCamName, (int)webCamResolution.x, (int)webCamResolution.y);
+      }
 
-        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
+      Debug.Log(webCamTexture.deviceName);
+
+      webCamTexture.Play();
     }
 
-    void Update()
-    {
-        if(staticInput != null) 
-            return;
-        
-        if(!webCamTexture.didUpdateThisFrame) 
-            return;
+    inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
+  }
 
-        var aspect1 = (float)webCamTexture.width / webCamTexture.height;
-        var aspect2 = (float)inputRT.width / inputRT.height;
-        var aspectGap = aspect2 / aspect1;
+  void Update()
+  {
+    if (staticInput != null)
+      return;
 
-        var vMirrored = webCamTexture.videoVerticallyMirrored;
-        var scale = new Vector2(aspectGap, vMirrored ? -1 : 1);
-        var offset = new Vector2((1 - aspectGap) / 2, vMirrored ? 1 : 0);
+    if (!webCamTexture.didUpdateThisFrame)
+      return;
 
-        Graphics.Blit(webCamTexture, inputRT, scale, offset);
-        
-        OnFrameReady.Invoke(inputImageTexture);
-    }
+    var aspect1 = (float)webCamTexture.width / webCamTexture.height;
+    var aspect2 = (float)inputRT.width / inputRT.height;
+    var aspectGap = aspect2 / aspect1 * -1;
 
-    void OnDestroy(){
-        if (webCamTexture != null) Destroy(webCamTexture);
-        if (inputRT != null) Destroy(inputRT);
-    }
+    var vMirrored = webCamTexture.videoVerticallyMirrored;
+    var scale = new Vector2(aspectGap, vMirrored ? -1 : 1);
+    var offset = new Vector2((1 - aspectGap) / 2, vMirrored ? 1 : 0);
+
+    Graphics.Blit(webCamTexture, inputRT, scale, offset);
+
+    OnFrameReady.Invoke(inputImageTexture);
+  }
+
+  void OnDestroy()
+  {
+    if (webCamTexture != null) Destroy(webCamTexture);
+    if (inputRT != null) Destroy(inputRT);
+  }
 }
