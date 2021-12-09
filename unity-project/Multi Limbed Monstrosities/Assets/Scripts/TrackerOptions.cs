c6369ba36996232cc2128c1f9ff379a2edd372db
James Camilleri <james@james.mt>
2021-12-09 14:35:22 +0000

    Update MQTT to handle player number

---
+++ unity-project/Multi Limbed Monstrosities/Assets/Scripts/TrackerOptions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerOptions : MonoBehaviour
+{
+  public GameObject playerConfig;
+
+  [HideInInspector]
+  public int playerNumber;
+
+  void Awake()
+  {
+    playerNumber = playerConfig.GetComponent<PlayerConfig>().playerNumber;
+    Debug.Log("Player " + playerNumber);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
